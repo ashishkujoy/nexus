1fa21c5f305a918642cabdc17042c28bda919299
ashishku <ashishkujoy@gmail.com>
2025-06-28 15:21:17 +0530

    Visits intern page on intern row click

--- src/app/batch/[batchId]/page.tsx
+++ src/app/batch/[batchId]/page.tsx
@@ -1,11 +1,12 @@
-import { FeedbackIcon, InternIcon, NoticeIcon, ObservationIcon } from "@/app/components/Icons";
+import { FeedbackIcon, InternIcon, ObservationIcon } from "@/app/components/Icons";
 
 import AppHeader from "@/app/components/AppHeader";
+import { Skeleton } from "@/app/components/Skeleton";
 import { ReactNode, Suspense } from "react";
+import { fetchBatch, fetchInterns } from "./action";
 import BatchPageHeader from "./BatchPageHeader";
 import "./page.css";
-import { fetchBatch, fetchInterns } from "./action";
-import { Skeleton } from "@/app/components/Skeleton";
+import InternRow from "./InternRow";
 
 type QuickActionProps = {
     title: string;
@@ -105,51 +106,6 @@ const RecentActivities = () => {
     )
 }
 
-type InternRowProps = {
-    name: string;
-    colorCode?: string;
-    notice: boolean;
-}
-
-const NoticeBadge = () => {
-    return (
-        <div className="notice-container">
-            <NoticeIcon />
-        </div>
-    )
-}
-
-const Status = (props: { colorCode?: string; notice: boolean }) => {
-    return (
-        <td className="table-cell">
-            <div style={{ display: "flex", alignItems: "center", gap: "8px" }}>
-                <span className={`color-badge color-${props.colorCode || "none"}`}></span>
-                {props.notice && <NoticeBadge />}
-
-            </div>
-        </td>
-    )
-}
-
-const InternRow = (props: InternRowProps) => {
-    return (
-        <tr className="intern-row">
-            <td>
-                <div className="intern-name">
-                    <div>
-                        <div style={{ fontWeight: "500" }}>{props.name}</div>
-                    </div>
-                </div>
-            </td>
-            <Status colorCode={props.colorCode} notice={props.notice} />
-            <td>
-                <button className="btn btn-secondary"
-                    style={{ padding: "4px 8px", fontSize: "12px" }}>View</button>
-            </td>
-        </tr>
-    )
-}
-
 const LoadingRow = () => {
     return (
         <tr className="intern-row">
@@ -183,6 +139,7 @@ const InternRows = async (props: { batchId: number }) => {
             {
                 interns.map(intern => <InternRow
                     key={intern.id}
+                    id={intern.id}
                     name={intern.name}
                     colorCode={intern.colorCode || undefined}
                     notice={intern.notice}
